13addbb46faf670c4f7c53e3e48687d808d651ff
funsooje <funsooje@gmail.com>
2021-09-29 12:11:39 -0700

    Base

---
+++ csprj5.2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace csprj5._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
